f6f5656460273d149c93f31a26c02e22c44fee56
Jalal Hejazi <jalal@superusers.dk>
2019-09-12 19:08:52 +0200

    swagger integration

--- routes/weather.js
+++ routes/weather.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 var vejret      = require('../lib/weather');
 
+// https://github.com/Surnet/swagger-jsdoc/blob/master/docs/GETTING-STARTED.md
 
+/**
+ * @swagger
+ *
+ * /api/v1/weather:
+ *   get:
+ *     description: Weather Info
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: weather
+ */
 router.get('/', (req, res, next) => {
   vejret( data =>  res.json(data) )
 });
