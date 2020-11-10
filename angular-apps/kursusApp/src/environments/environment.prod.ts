6bed2810f757646671e9cd3482f6bada40b78a1a
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-11-10 20:04:00 +0100

    fix

--- angular-apps/kursusApp/src/environments/environment.prod.ts
+++ angular-apps/kursusApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    courses : 'https://superusers-kursus-angular-2020.azurewebsites.net/api/v1/kursus',
+    courses     : 'https://superusers-kursus-angular-2020.azurewebsites.net/api/v2/kursus',
     courseInfo  : 'https://service.superusers.dk/DataService/Kursus/Oversigt'
   }
 };
